2865d1f68d1cb08db048b18a383f036e752f8bfc
reactive-proc-1585 <jakbellamy@gmail.com>
2018-10-14 19:47:26 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
 # Build your say_hello method here
 def say_hello(array)
-  array = ["Gabriela"]
   array.each do |name|
     if name == "Gabriela"
       puts "Hello #{name}!"
@@ -10,5 +9,6 @@ def say_hello(array)
   end
 end
 
+array = ["Gabriela"]
 
 say_hello(array)
